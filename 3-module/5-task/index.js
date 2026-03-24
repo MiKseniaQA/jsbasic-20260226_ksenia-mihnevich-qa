94a8857bc847a378103666c6f1350e37478f4aa2
Ksenia Mihnevich <ksenia.mihnevich.qa@gmail.com>
2026-03-24 06:10:52 +0300

    solution of function getMinMax

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
-  // ваш код...
+  const numbers = str
+  .split(' ') 
+  .filter(num => !Number.isNaN(parseFloat(num)) )  
+  .map(item=> parseFloat(item))         
+  return{
+    min: Math.min(...numbers),
+    max: Math.max(...numbers)
+  }
 }
